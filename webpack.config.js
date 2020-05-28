60fb882c2b023341faa6d39c3369ad4a95359a4e
syu-y <syu12580526@gmail.com>
2020-05-28 23:43:50 +0900

    webpackの設定

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: slsw.lib.webpack.isLocal
+    ? 'cheap-module-eval-source-map'
+    : 'source-map',
+  output: {
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+    path: path.join(__dirname, '.webpack'),
+  },
+  target: 'node',
+  externals: ['chrome-aws-lambda', 'aws-sdk'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // include .js files
+        enforce: 'pre', // preload the jshint loader
+        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        include: __dirname,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src/'),
+    },
+  },
+};
