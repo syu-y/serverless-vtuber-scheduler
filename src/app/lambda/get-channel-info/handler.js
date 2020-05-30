91412427b42c8a0bc3fd67f64de6b7f9790ca36e
syu-y <syu12580526@gmail.com>
2020-05-30 21:11:05 +0900

    vtuberのチャンネルIDから情報を更新する処理作成

--- src/app/lambda/get-channel-info/handler.js
+++ src/app/lambda/get-channel-info/handler.js
@@ -1,6 +1,35 @@
-import dayjs, { isSameOrAfter } from 'dayjs';
 import youtubeApi from 'src/api/youtube';
-import channelIdList from 'src/app/constants/channel-id-list';
-import broadcastInfoRepository from 'src/db/dynamodb/broadcast-info';
+import { channelIdList } from 'src/app/constants/channel-id-list';
+import channelInfoRepository from 'src/db/dynamodb/channel-info';
 
-export const getChannelInfo = async () => {};
+export const getChannelInfo = async () => {
+  await Promise.all(
+    channelIdList.map(async (channelId) => {
+      try {
+        // チャンネル情報を取得
+        const apiResponse = await youtubeApi.getChannelInfo(channelId);
+        console.log(apiResponse);
+        if (!apiResponse.data) return;
+
+        const allData = apiResponse.data.items[0];
+
+        // 登録用のオブジェクト生成
+        const channelInfo = {
+          channelId: channelId,
+          name: allData.snippet.title,
+          iconUrl: allData.snippet.thumbnails.default.url,
+        };
+
+        // DBに登録
+        channelInfoRepository.putItem(channelInfo);
+      } catch (error) {
+        console.log(error);
+        return {
+          statusCode: error.statusCode || 500,
+          body: error.message,
+        };
+      }
+    }),
+  );
+  return { statusCode: 200 };
+};
--- src/app/lambda/get-youtube-broadcast/handler.js
+++ src/app/lambda/get-youtube-broadcast/handler.js
@@ -1,4 +1,4 @@
-import dayjs, { isSameOrAfter } from 'dayjs';
+import dayjs from 'dayjs';
 import youtubeApi from 'src/api/youtube';
 import channelInfoRepository from 'src/db/dynamodb/channel-info';
 import broadcastInfoRepository from 'src/db/dynamodb/broadcast-info';
@@ -18,6 +18,7 @@ export const getYoutubeBloadcast = async () => {
       try {
         // 動画情報を取得
         const apiResponse = await youtubeApi.getBroadcasts({ channelId });
+        console.log(apiResponse);
         if (!apiResponse.data) return;
 
         // 配信情報のリスト生成
@@ -31,31 +32,39 @@ export const getYoutubeBloadcast = async () => {
         });
 
         // 動画IDをキーに各動画の配信日を取得する
-        broadcasts.map(async (broadcast) => {
-          const apiResponse = await youtubeApi.getBroadcastSchedule(
-            broadcast.videoId,
-          );
-          if (!apiResponse.data) return;
 
-          // 配信日取得
-          const date = dayjs(
-            apiResponse.data.items[0].liveStreamingDetails.scheduledStartTime,
-          );
+        await Promise.all(
+          broadcasts.map(async (broadcast) => {
+            const apiResponse = await youtubeApi.getBroadcastSchedule(
+              broadcast.videoId,
+            );
+            if (!apiResponse.data) return;
 
-          // 配信日がlambda動作日以降の場合、Dynamoに格納
-          const today = dayjs().format();
-          if (date.isAfter(today)) {
-            const broadcastDate = date.format();
-            // 登録情報に配信日を追加
-            broadcast = { ...broadcast, broadcastDate };
-            console.log(broadcast);
-          }
-          // DBに登録
-          await broadcastInfoRepository.putItem(broadcast);
-        });
+            // 配信日取得
+            const date = dayjs(
+              apiResponse.data.items[0].liveStreamingDetails.scheduledStartTime,
+            );
+
+            // 配信日がlambda動作日以降の場合、Dynamoに格納
+            const today = dayjs().format();
+            if (date.isAfter(today)) {
+              const broadcastDate = date.format();
+              // 登録情報に配信日を追加
+              broadcast = { ...broadcast, broadcastDate };
+              console.log(broadcast);
+            }
+            // DBに登録
+            broadcastInfoRepository.putItem(broadcast);
+          }),
+        );
       } catch (error) {
         console.log(error);
+        return {
+          statusCode: error.statusCode || 500,
+          body: error.message,
+        };
       }
     }),
   );
+  return { statusCode: 200 };
 };
